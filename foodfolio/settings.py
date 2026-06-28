bd8eed104ee7d7529cfacc23584ecf168ca88c1f
Akshay <akshaykv534@.com>
2026-06-28 21:06:33 +0530

    Configure project for Render deployment

--- foodfolio/settings.py
+++ foodfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ot^5meav9pv3nh##f7_1pr*$61(osot+)#6klhgo4zcb358_$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
